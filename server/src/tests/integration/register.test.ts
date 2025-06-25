cb803cff8f8ed2eab3a8bec586028d5172e73f73
Caleb Yohannes <c.yohan21@outlook.com>
2025-06-25 15:37:57 -0400

    re-organized

--- server/src/tests/integration/register.test.ts
+++ server/src/tests/integration/register.test.ts
@@ -1,5 +1,17 @@
 import request from 'supertest';
 import app from '../../app';
+import prisma from "../../lib/prisma"
+
+beforeAll(async () => {
+    await prisma.user.deleteMany();
+    const res = await request(app)
+    .post('/api/auth/register')
+    .send({email: 'caleb3@example.com', password: '3'})
+})
+
+afterAll(async () => {
+    await prisma.$disconnect();
+})
 
 describe('POST /api/auth/register', () => {
     it('should return error: An email and password are both required.', async () => {
@@ -14,6 +26,13 @@ describe('POST /api/auth/register', () => {
         .post('/api/auth/register')
         .send({email: '', password: '12345'})
         expect(res.status).toBe(400)
+        expect(res.body).toHaveProperty('error')
+}); 
+    it('should return error: User already registered', async () => {
+        const res = await request(app)
+        .post('/api/auth/register')
+        .send({email: 'caleb3@example.com', password: '3'})
+        expect(res.status).toBe(409)
         expect(res.body).toHaveProperty('error')
 }); 
     it('should return 200 status: User added', async () => {
