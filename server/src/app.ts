2eb1fc274006507c73e50b584ec1ac8f3315f7d2
Caleb Yohannes <c.yohan21@outlook.com>
2025-06-27 18:18:29 -0400

    added helmet (extra protection)

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import authRouter from "./routes/authRouter";
 import cookieParser from "cookie-parser";
 import { errorHandler } from "./middleware/errorHandler";
+import helmet from "helmet";
 
 const app = express();
 const frontendPort = 3000;
@@ -16,5 +17,6 @@ app.use(express.json());
 app.use(cookieParser()); // Needed to get refresh tokens from req.cookies.refreshToken
 app.use("/api/auth", authRouter);
 app.use(errorHandler);
+app.use(helmet());
 
 export default app;
